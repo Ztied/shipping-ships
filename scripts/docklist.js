8a6f441782b5fdb814c3a5454037d7e705c2ed2b
Michael <michaeljrdeitz@gmail.com>
2024-02-05 15:38:45 -0600

    dockslists wont pull the right id

--- scripts/docklist.js
+++ scripts/docklist.js
@@ -1,16 +1,62 @@
-import { getDocks } from "./database.js"
+import { getDocks, getHaulers } from "./database.js"
 
 
 export const DockList = () => {
     const docks = getDocks()
+    const haulers = getHaulers()
+    document.addEventListener (
+        "click",
+        (clickEvent) => {
+            const itemClicked = clickEvent.target
 
+            const dockId = itemClicked.dataset.id
+
+            let dock = ""
+
+            const allDocks = getDocks()
+            for(const docks of allDocks) {
+                if(docks.id === parseInt(dockId)) {
+                    dock = docks.location
+                }
+            }
+
+            const haulersId = itemClicked.dataset.haulid
+
+            const hauler = getHaulers()
+
+            let hauls = ""
+
+            for(const haul of hauler) {
+                if(haul.id === parseInt(haulersId)) {
+                    hauls = haul.name
+                }
+            }
+            window.alert(`The ${dock} is currently unloading ${hauls}`)
+        }
+    )
+    
+    
     let docksHTML = "<ul>"
-
+        let hauls = []
     for (const dock of docks) {
-        docksHTML += `<li>${dock.location}</li>`
+       for(const haul of haulers){
+             hauls = haul.id
+        }
+    
+    docksHTML += `<li data-id=${dock.id} data-haulId=${hauls}>${dock.location} can hold ${dock.volume} million tons of cargo</li>`
+        
     }
 
     docksHTML += "</ul>"
 
     return docksHTML
 }
+
+// for (const dock of docks) {
+        
+// }
+// for(const haul of haulers){
+//     hauls = haul.id
+// } 
+
+// docksHTML += `<li data-id=${dock.id} data-haulId=${hauls}>${dock.location} can hold ${dock.volume} million tons of cargo</li>`
--- scripts/shipping.js
+++ scripts/shipping.js
@@ -9,24 +9,36 @@ document.addEventListener(
     (clickEvent) => {
         const itemClicked = clickEvent.target
         
-        const haulersId = itemClicked.dataset.haulerForeignKey
+        const shipperId = itemClicked.dataset.id
 
-        // let haulingShip = { name: ""}
+        let ships = ""
 
-        const hauler = getShippingShips() 
-        for(const haul of hauler) {
-            if(haul.id === parseInt(haulersId)){
-                window.alert(` is being hauled by ${haul.name}`)
+        const shipper = getShippingShips() 
+        for(const ship of shipper) {
+            if(ship.id === parseInt(shipperId)){
+                ships = ship.name
             }
             
         }
+        const haulersId = itemClicked.dataset.haulerforeignkey
+        
+        const hauler = getHaulers() 
+
+        let hauls = ""
+
+        for(const haul of hauler) {
+            if(haul.id === parseInt(haulersId)){
+                hauls = haul.name
+            }
+        } 
+        window.alert(`${ships} is being hauled by ${hauls}`)
     }
 )
 
     let shippersHTML = "<ul>"
 
     for (const ship of shippers) {
-        shippersHTML += `<li data-haulerForeignKey=${ship.haulersId}>${ship.name}</li>`
+        shippersHTML += `<li    data-id=${ship.id} data-haulerForeignKey=${ship.haulersId}>${ship.name}</li>`
     }
 
     shippersHTML += "</ul>"
